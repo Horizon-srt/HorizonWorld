00eb34ba87e444119b359910860dd07bd2ee353d
Horizon_srt <horizon_srt@outlook.com>
2022-04-01 14:58:58 +0800

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-7gbws1jsebcb7e6a","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-7gbws1jsebcb7e6a --n quickstartFunctions --r --project "D:\WeChatSmallPro\HorizonWorld" --report_first --report
